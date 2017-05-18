e1ea407717afb85feb3905215ac9571d56c6215e
valentina lopez <valentina lopez>
2017-05-18 00:43:38 -0400

    creacion de función tarjetas, que borra texto y crea un nuevo text area para escribir tareas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 function borrar(){
-	var parent = document.getElementById("input-box");
-	var child = document.getElementById("input-lista");
-	var container = document.getElementById("input-box");
+	var inputContainer = document.getElementById("input-box");
+	var input = document.getElementById("input-lista");
 	var textArea = document.createElement("textarea");
 
-	parent.removeChild(child);
+	inputContainer.removeChild(input);
 
 	textArea.setAttribute("placeholder","Añadir una lista");
 	textArea.setAttribute("id","fresh-list");
 	textArea.classList.add("text");
-	container.appendChild(textArea);
+	inputContainer.appendChild(textArea);
 
 	var buttonContainer = document.getElementById("button-area");
 	var button = document.createElement("button");
@@ -18,7 +17,50 @@ function borrar(){
 	button.setAttribute("name", "guardar");
 	button.innerHTML = "Guardar";
 	buttonContainer.appendChild(button);
+
+	document.getElementById("boton").addEventListener("click", function(){
+		document.getElementById("nombre-lista").innerHTML = document.getElementById("fresh-list").value;
+		var inputList= document.getElementById("fresh-list");
+		inputContainer.removeChild(inputList);
+		
+		var buttonContainer = document.getElementById("button-area")
+		var boton= document.getElementById("boton");
+		buttonContainer.removeChild(boton);
+
+		var enlace = document.createElement("p");
+		var text = document.createTextNode("Agrega una tarjeta...");
+
+		enlace.setAttribute("id","tarjetas");
+		enlace.appendChild(text);
+		inputContainer.appendChild(enlace);
+
+		document.getElementById("tarjetas").addEventListener("mouseover", tarjetas);
+	})
 }
+
+function tarjetas(){
+	var enlace = document.getElementById("tarjetas");
+	var inputContainer = document.getElementById("nombre-lista");
+	var textArea = document.createElement("textarea");
+	enlace.remove(inputContainer);
+
+	textArea.setAttribute("placeholder", "Escribe tu tarea aquí");
+	var container = document.getElementById("input-box")
+	container.appendChild(textArea);
+
+	var boton= document.createElement("button");
+	boton.setAttribute("id","boton-guardar");
+	var buttonContainer = document.getElementById("button-area");
+	
+	var button = document.createElement("button");
+
+	button.setAttribute("id","boton");
+	button.setAttribute("name", "guardar");
+	button.innerHTML = "Guardar";
+	buttonContainer.appendChild(button);
+}
+
+
 
 
 
